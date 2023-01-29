ee0c474bc7ba0f24da812a1c6eb04885a8c5ee9e
Mary Becker <mary.becker@ct.gov>
2023-01-29 17:59:15 -0500

    added in total cyano count data

--- main.js
+++ main.js
@@ -27,6 +27,7 @@ L.control.attribution({position: 'bottomleft'}).addTo(map);
 L.control.zoom({position:'topright'}).addTo(map);
 L.control.layers(baseMaps).addTo(map);
 
+// Web service query build
 var base = "https://services5.arcgis.com/ffJESZQ7ml5P9xH7/ArcGIS/rest/services/"
 var s123 = "survey123_2bd9b97d23124dbfae7df325f106039b_stakeholder/"
 var fsrv = "FeatureServer/0/"
@@ -46,64 +47,82 @@ console.log(burl);
 //https://base_service(base)/survey_1_2_3/FeatureServer/0/ #attachment_id# (200) /attachments/ #image_id# (130)
 
 d3.json("data/ctStateBoundary.geojson").then(function(bound){
-    d3.json(burl).then(function(bloomwatchData){
-        var img = {};
-        ctFeatures = [];
-    
-        for(var i=0; i<bloomwatchData['features'].length; i++){
-            if(bloomwatchData['features'][i]['properties']['stateprov'] == 'CT'){
-                ctFeatures.push(bloomwatchData['features'][i])
-            }
-        }
-    
-        var ctBloomwatch = {type: 'FeatureCollection', features: ctFeatures};
-        //console.log(ctBloomwatch);
-    
-        ctBloomwatch['features'][0]['id'] //one way
-        ctBloomwatch['features'][0]['properties']['objectid'] //second way
-    
-        var global_to_aid = {};
-        var aid_to_idx    = {};
-        var uimgs = [];
-        for(var i=0; i<ctBloomwatch['features'].length;i++){
-            var aid = ctBloomwatch['features'][i]['id'];
-            global_to_aid[ctBloomwatch['features'][i]['properties']['globalid']] = aid;
-            aid_to_idx[aid] = i;
-            var uimg = base + s123 + fsrv + aid +"/attachments/?f=pjson&token="
-            uimgs.push(d3.json(uimg));
-            ctBloomwatch['features'][i]['properties']['iids'] = []; 
-        } 
-    
-        //console.log(global_to_aid);
-        //console.log(aid_to_idx);
-        Promise.all(uimgs).then(resolve_junk_response);
-    
-        function resolve_junk_response(junk_data){
-            //console.log(junk_data);
-            for(var i=0; i<junk_data.length;i++){
-                for(var j=0; j<junk_data[i]['attachmentInfos'].length;j++){
-                    var gid = junk_data[i]['attachmentInfos'][j]['parentGlobalId'];
-                    var iid = junk_data[i]['attachmentInfos'][j]['id'];
-                    var aid = global_to_aid[gid];
-                    ctBloomwatch['features'][aid_to_idx[aid]]['properties']['iids'].push(iid);
+    d3.csv("data/cell_cnt.csv").then(function(cellCnt){
+        d3.json(burl).then(function(bloomwatchData){
+            var img = {};
+            ctFeatures = [];
+        
+            for(var i=0; i<bloomwatchData['features'].length; i++){
+                if(bloomwatchData['features'][i]['properties']['stateprov'] == 'CT'){
+                    ctFeatures.push(bloomwatchData['features'][i])
                 }
             }
-            console.log(ctBloomwatch);
-            console.log(d3.timeFormat("%Y-%m-%d")((ctBloomwatch['features'][92]['properties']['obsdate'])));
-            //map layer drawing must go here, this is the synchronized section, AKA the end!!!
-            //leaflet code here please.....
-            drawMap(ctBloomwatch)
-
-            // load GeoJSON of CT Boundary
-            var linestyle = {"color": "#333333","weight": 2,};
-            L.geoJson(bound,{style:linestyle}).addTo(map);
-    
-          
-        }
-    
         
-        //},function(img_err){ console.log('img error...')} ); //img_data call
-    },function(burl_err){ console.log('burl='+burl+' error...')}) //burl call
+            var ctBloomwatch = {type: 'FeatureCollection', features: ctFeatures};
+            //console.log(ctBloomwatch);
+        
+            ctBloomwatch['features'][0]['id'] //one way
+            ctBloomwatch['features'][0]['properties']['objectid'] //second way
+        
+            var global_to_aid = {};
+            var aid_to_idx    = {};
+            var uimgs = [];
+            for(var i=0; i<ctBloomwatch['features'].length;i++){
+                var aid = ctBloomwatch['features'][i]['id'];
+                global_to_aid[ctBloomwatch['features'][i]['properties']['globalid']] = aid;
+                aid_to_idx[aid] = i;
+                var uimg = base + s123 + fsrv + aid +"/attachments/?f=pjson&token="
+                uimgs.push(d3.json(uimg));
+                ctBloomwatch['features'][i]['properties']['iids'] = [];
+            } 
+        
+            //console.log(global_to_aid);
+            //console.log(aid_to_idx);
+            Promise.all(uimgs).then(resolve_junk_response);
+        
+            function resolve_junk_response(junk_data){
+                //console.log(junk_data);
+                for(var i=0; i<junk_data.length;i++){
+                    for(var j=0; j<junk_data[i]['attachmentInfos'].length;j++){
+                        var gid = junk_data[i]['attachmentInfos'][j]['parentGlobalId'];
+                        var iid = junk_data[i]['attachmentInfos'][j]['id'];
+                        var aid = global_to_aid[gid];
+                        ctBloomwatch['features'][aid_to_idx[aid]]['properties']['iids'].push(iid);
+                    }
+                }
+
+                for(var i=0; i<ctBloomwatch.features.length;i++){ 
+                    for(var j=0; j<cellCnt.length;j++){
+                        if(ctBloomwatch['features'][i]['properties']['objectid'] === Number(cellCnt[j]['objectid'])){
+                            ctBloomwatch['features'][i]['properties']['cellCnt'] = cellCnt[j]['cell_cnt_mgl']
+                        }
+                        // else if(ctBloomwatch['features'][i]['properties']['objectid'] != Number(cellCnt[j]['objectid'])){
+                        //     ctBloomwatch['features'][i]['properties']['cellCnt'] = 'Not Available'
+                        // }
+                    }
+                }
+
+                for(var i=0; i<ctBloomwatch.features.length;i++){ 
+                    if(ctBloomwatch['features'][i]['properties']['cellCnt'] ==  undefined){
+                        ctBloomwatch['features'][i]['properties']['cellCnt'] = 'Not Available'
+                    }
+                }
+                console.log(ctBloomwatch);
+                console.log(cellCnt);
+                //map layer drawing must go here, this is the synchronized section, AKA the end!!!
+                //leaflet code here.....
+                drawMap(ctBloomwatch)
+    
+                // load GeoJSON of CT Boundary
+                var linestyle = {"color": "#333333","weight": 2,};
+                L.geoJson(bound,{style:linestyle}).addTo(map);
+        
+              
+            }
+        
+            //},function(img_err){ console.log('img error...')} ); //img_data call
+        },function(burl_err){ console.log('burl='+burl+' error...')}) //burl call
+    })    
 });
 
 
@@ -153,41 +172,52 @@ function drawMap(ctBloomwatch){
 
                         if(props['iids'].length == 1){
                             var popupContent = `<b>${props['lakeName']}</b></br>
-                            ${props['iids'][0]},${props['iids'][1]},${props['iids'][2]}</br>
                             Observed Date: ${fdate(props['obsdate'])}</br>
                             Observed Bloom: ${props['bloomExtent']}</br>
-                            <img id='popupImg' src='${iurl}' alt = ${props['descriptPic1']}>`;
+                            Cyanobacteria Cell Count (cells/mL): ${props['cellCnt']}</br>
+                            <a href='${iurl}' target="_blank">
+                            <img id='popupImg' src='${iurl}' alt = ${props['descriptPic1']}></a></br>
+                            <i>Click on an image to enlarge</i>`;
                         }
                         else if(props['iids'].length == 2){
                             var popupContent = `<b>${props['lakeName']}</b></br>
-                            ${props['iids'][0]},${props['iids'][1]},${props['iids'][2]}</br>
                             Observed Date: ${fdate(props['obsdate'])}</br>
                             Observed Bloom: ${props['bloomExtent']}</br>
-                            <img id='popupImg' src='${iurl}' alt = ${props['descriptPic1']}>
-                            <img id='popupImg' src='${iurl2}' alt = ${props['descriptPic2']}>`;
+                            Cyanobacteria Cell Count (cells/mL): ${props['cellCnt']}</br>
+                            <a href='${iurl}' target="_blank">
+                            <img id='popupImg' src='${iurl}' alt = ${props['descriptPic1']}></a>
+                            <a href='${iurl2}' target="_blank">
+                            <img id='popupImg' src='${iurl2}' alt = ${props['descriptPic2']}></a></br>
+                            <i>Click on an image to enlarge</i>`;
                         }
                         else if(props['iids'].length == 3){
                             var popupContent = `<b>${props['lakeName']}</b></br>
-                            ${props['iids'][0]},${props['iids'][1]},${props['iids'][2]}</br>
                             Observed Date: ${fdate(props['obsdate'])}</br>
                             Observed Bloom: ${props['bloomExtent']}</br>
-                            <img id='popupImg' src='${iurl}' alt = ${props['descriptPic1']}>
-                            <img id='popupImg' src='${iurl2}' alt = ${props['descriptPic2']}>
-                            <img id='popupImg' src='${iurl3}' alt = ${props['descriptPic3']}>`;
+                            Cyanobacteria Cell Count (cells/mL): ${props['cellCnt']}</br>
+                            <a href='${iurl}' target="_blank">
+                            <img id='popupImg' src='${iurl}' alt = ${props['descriptPic1']}></a>
+                            <a href='${iurl2}' target="_blank">
+                            <img id='popupImg' src='${iurl2}' alt = ${props['descriptPic2']}></a>
+                            <a href='${iurl3}' target="_blank">
+                            <img id='popupImg' src='${iurl3}' alt = ${props['descriptPic3']}></a></br>
+                            <i>Click on an image to enlarge</i>`;
                         }
                         else{
                             var popupContent = `<b>${props['lakeName']}</b></br>
-                            ${props['iids'][0]},${props['iids'][1]},${props['iids'][2]}</br>
                             Observed Date: ${fdate(props['obsdate'])}</br>
                             Observed Bloom: ${props['bloomExtent']}</br>
-                            <img id='popupImg' src='images/noImage.png' alt = 'No Image Available'>`
+                            Cyanobacteria Cell Count (cells/mL): ${props['cellCnt']}</br>
+                            <img id='popupImg' src='images/noImage.png' alt = 'No Image Available'></br>
+                            <i>Click on an image to enlarge</i>`
                         }
 
                         
                     
                         // // bind a tooltip to layer with county-specific information
                         layer.bindPopup(popupContent, {
-                            maxWidth: 500
+                            maxWidth: 400,
+                            className: 'customPopup'
                         });
 
                         layer.on('click', function(e){
@@ -213,39 +243,80 @@ function getColor(date, sdate, edate, bloom){
 
 function addSliderInteraction(sites){
 
-    $( "#slider-range" ).on( "slide", function( event, ui ) {
+    $("#slider-range" ).on( "slide", function( event, ui ) {
         var vals = ui.values;
 
         // loop through each county layer to update the color and tooltip info
-    sites.eachLayer(function (layer) {
+        sites.eachLayer(function (layer) {
 
-        const props = layer.feature.properties;
-        var fdate = d3.timeFormat("%Y-%m-%d");
-        var iurl = base + s123 + fsrv + props['objectid'] + "/attachments/" + props['iids'][0];
+            var fdate = d3.timeFormat("%Y-%m-%d");
+            var props = layer.feature.properties;
+            var iurl  = base + s123 + fsrv + props['objectid'] + "/attachments/" + props['iids'][0];
+            var iurl2 = base + s123 + fsrv + props['objectid'] + "/attachments/" + props['iids'][1];
+            var iurl3 = base + s123 + fsrv + props['objectid'] + "/attachments/" + props['iids'][2];
 
-        // set the fill color of layer based on data value
-        layer.setStyle({
-            radius: bloomSize(props['obsdate'], vals[0], vals[1], props['bloomExtent']),
-            fillColor: getColor(props['obsdate'], vals[0], vals[1], props['bloomExtent'])
-        });
 
-        let popupContent = `<b>${props['lakeName']}</b></br>
-                        Observed Date: ${fdate(props['obsdate'])}</br>
-                        Observed Bloom: ${props['bloomExtent']}</br>
-                        <img src="${iurl}" alt = "No Image Available" style="width:200px">`;
-                    
-                        // // bind a tooltip to layer with county-specific information
-                        layer.bindPopup(popupContent, {
-                            maxWidth: 200
-                        });
+            // set the fill color of layer based on data value
+            layer.setStyle({
+                radius: bloomSize(props['obsdate'], vals[0], vals[1], props['bloomExtent']),
+                fillColor: getColor(props['obsdate'], vals[0], vals[1], props['bloomExtent'])
+            });
 
-                        layer.on('click', function(e){
-                            map.setView(e.target.getLatLng(), 9)
-                        });
-                    
-    }); 
+            if(props['iids'].length == 1){
+                var popupContent = `<b>${props['lakeName']}</b></br>
+                Observed Date: ${fdate(props['obsdate'])}</br>
+                Observed Bloom: ${props['bloomExtent']}</br>
+                Cyanobacteria Cell Count (cells/mL): ${props['cellCnt']}</br>
+                <a href='${iurl}' target="_blank">
+                <img id='popupImg' src='${iurl}' alt = ${props['descriptPic1']}></a></br>
+                <i>Click on an image to enlarge</i>`;
+            }
+            else if(props['iids'].length == 2){
+                var popupContent = `<b>${props['lakeName']}</b></br>
+                Observed Date: ${fdate(props['obsdate'])}</br>
+                Observed Bloom: ${props['bloomExtent']}</br>
+                Cyanobacteria Cell Count (cells/mL): ${props['cellCnt']}</br>
+                <a href='${iurl}' target="_blank">
+                <img id='popupImg' src='${iurl}' alt = ${props['descriptPic1']}></a>
+                <a href='${iurl2}' target="_blank">
+                <img id='popupImg' src='${iurl2}' alt = ${props['descriptPic2']}></a></br>
+                <i>Click on an image to enlarge</i>`;
+            }
+            else if(props['iids'].length == 3){
+                var popupContent = `<b>${props['lakeName']}</b></br>
+                Observed Date: ${fdate(props['obsdate'])}</br>
+                Observed Bloom: ${props['bloomExtent']}</br>
+                Cyanobacteria Cell Count (cells/mL): ${props['cellCnt']}</br>
+                <a href='${iurl}' target="_blank">
+                <img id='popupImg' src='${iurl}' alt = ${props['descriptPic1']}></a>
+                <a href='${iurl2}' target="_blank">
+                <img id='popupImg' src='${iurl2}' alt = ${props['descriptPic2']}></a>
+                <a href='${iurl3}' target="_blank">
+                <img id='popupImg' src='${iurl3}' alt = ${props['descriptPic3']}></a></br>
+                <i>Click on an image to enlarge</i>`;
+            }
+            else{
+                var popupContent = `<b>${props['lakeName']}</b></br>
+                Observed Date: ${fdate(props['obsdate'])}</br>
+                Observed Bloom: ${props['bloomExtent']}</br>
+                Cyanobacteria Cell Count (cells/mL): ${props['cellCnt']}</br>
+                <img id='popupImg' src='images/noImage.png' alt = 'No Image Available'></br>
+                <i>Click on an image to enlarge</i>`
+            }
+                        
+            //bind a tooltip to layer 
+            layer.bindPopup(popupContent, {
+                maxWidth: 400,
+                className: 'customPopup'
+            });
+
+            layer.on('click', function(e){
+                map.setView(e.target.getLatLng(), 9)
+            });
+                        
+        }); 
         
-      } );
+      });
     
 };
 
